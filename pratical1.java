d3aa85cd323d2ccfbc43a8ca29d702dd0b47a1a9
Tharun46star <tharundon950@gmail.com>
2026-08-07 10:03:02 +0530

    Update pratical1.java

--- pratical1.java
+++ pratical1.java
@@ -65,14 +65,21 @@ return length * width;
 
 class Square extends Rectangle
 {
+double sideLength;
 public Square(Point p1, double sideLength)
 {
 super(p1, new Point(p1.getX() + sideLength, p1.getY()), new Point(p1.getX() +
 sideLength, p1.getY() + sideLength), new Point(p1.getX(), p1.getY() + sideLength));
+this.sideLength=sideLength;
+}
+public double calculateArea()
+{
+    return sideLength*sideLength;
+
 }
 
 }
-class practical1
+public class practical1
 {
 public static void main(String[] args)
 {
@@ -87,8 +94,6 @@ Square square = new Square(p1, 3);
 
 System.out.println("Area of trapezoid=" + trapezoid.calculateArea());
 System.out.println("Area of rectangle=" + rectangle.calculateArea());
-System.out.println("Area of square=" + square.calculateArea()); 
+System.out.println("Area of square=" + square.calculateArea());
 }
 }
-
-
